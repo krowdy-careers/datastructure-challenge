07fe747b894864f4c2eef5ea3a31efccf4bd68f2
christianTordoya03 <cristian14021997@gmail.com>
2022-11-25 05:08:56 -0500

    Create fetch for json and added other option

---
+++ fetch.js
@@ -0,0 +1,11 @@
+let myRequest = new Request("./input.json");
+
+let json = fetch(myRequest)
+.then(function(resp){
+    return resp.json();
+})
+.then(function(data){
+    console.log(data.cities);
+})
+
+export default json;
---
+++ index.js
@@ -0,0 +1,5 @@
+/*También se pudo traer de la siguiente manera*/
+/*import cities from "./input.json";*/
+import json from "./fetch.js";
+
+console.log(json.id)
