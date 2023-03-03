7bd29f34adb97f5cc069ac75ec5ffeea93a25f9d
RocioYC <111469698+RocioYC@users.noreply.github.com>
2023-03-03 14:28:23 -0500

    Delete comments

--- program.js
+++ program.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 
-// Leemos el archivo de entrada
 const rawData = fs.readFileSync('input.json');
 const data = JSON.parse(rawData);
 
-// Procesamos los datos y agrupamos las ciudades por clima similar
 const groupedData = data.cities.reduce((acc, city) => {
     const weather = city.weather;
     const key = `${weather.main}_${weather.description}`;
@@ -21,8 +19,6 @@ const groupedData = data.cities.reduce((acc, city) => {
     return acc;
 }, {});
 
-// Convertimos el objeto en un array para tener el formato deseado
 const output = Object.values(groupedData);
 
-// Escribimos el resultado en un archivo de salida en formato JSON
 fs.writeFileSync('output.json', JSON.stringify(output));
